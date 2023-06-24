fddc3026cf76a9b661c49b3de528d46951ba87ba
yashwanth-Echortech <128774280+yashwanth-Echortech@users.noreply.github.com>
2023-06-24 19:33:58 +0530

    chasijkbfd

---
+++ gsap.js
@@ -0,0 +1,35 @@
+// gsap.registerPlugin(ScrollTrigger);
+// const bodyScrollBar = Scrollbar.init(document.body,{
+//     damping:.1,
+//     delegateTo:document,
+//     alwaysShowTracks:true,
+//     speed:0.2,
+// })
+// ScrollTrigger.scrollerProxy("body",{
+//     scrollTop(value){
+//         if(arguments.length){
+//             bodyScrollBar.scrollTop = value;
+//         }
+//         return bodyScrollBar.scrollTop;
+//     }
+// });
+// bodyScrollBar.addListner(ScrollTrigger.update)
+// ScrollTrigger.defaults({
+//     scroller:document.body,
+// })
+// gsap.to(".gsapp",{
+//     backgroundPositionX:"0%",
+//     stagger:1,
+//     scrollTrigger:{
+//         trigger:"gsapp",
+//         scrub:1,
+//         start:"top center",
+//         end:"bottom top",
+
+//         }
+// })
+
+
+
+
+
