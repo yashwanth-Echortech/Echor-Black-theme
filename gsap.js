03d514b5a1127975e66f471b3c98cf0a123f4056
nikhil <nikhil@echortech.com>
2023-07-24 17:07:44 +0530

    pandu

--- gsap.js
+++ gsap.js
@@ -1,8 +1,8 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".c",{
-  scrollTrigger: ".c",
-  x:-500,
-  duration:3,
-  rotation:360,
-});
+// gsap.to(".c",{
+//   scrollTrigger: ".c",
+//   x:-500,
+//   duration:3,
+//   rotation:360,
+// });
